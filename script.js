2b99addd53559353ffc12606baf1f2346fc1bc53
nicolaiheuckHessel <101560263+nicolaiheuckHessel@users.noreply.github.com>
2024-04-17 21:37:12 +0200

    Converted to tampermonkey script

--- script.js
+++ script.js
@@ -1,86 +1,102 @@
-const searchPanelOpened = () => {
-  return document.getElementsByClassName("segment-builder-embed").length > 0;
-};
+// ==UserScript==
+// @name         Flowstack dropdown search
+// @namespace    https://app.flowstack.com
+// @version      0.1
+// @description  Add search functionality to flowstack dropdowns
+// @author       You
+// @match        https://app.flowstack.com/profiles
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=app.flowstack.com
+// @grant        none
+// ==/UserScript==
 
-const findElementByInnerText = (text) => {
-  var xpath = `//*[text()='${text}']`;
-  var matchingElement = document.evaluate(
-    xpath,
-    document,
-    null,
-    XPathResult.FIRST_ORDERED_NODE_TYPE,
-    null
-  ).singleNodeValue;
-  return matchingElement;
-};
+(async function() {
+    'use strict';
+    setTimeout(async () => {
+        const searchPanelOpened = () => {
+            return document.getElementsByClassName("segment-builder-embed").length > 0;
+        };
 
-const openSearchPanel = () => {
-  const searchButton = findElementByInnerText("Search");
-  searchButton.click();
-};
+        const findElementByInnerText = (text) => {
+            var xpath = `//*[text()='${text}']`;
+            var matchingElement = document.evaluate(
+                xpath,
+                document,
+                null,
+                XPathResult.FIRST_ORDERED_NODE_TYPE,
+                null
+            ).singleNodeValue;
+            return matchingElement;
+        };
 
-const loadScriptAsync = async (url) => {
-    const response = await fetch(url);
-    eval(await response.text());
-};
+        const openSearchPanel = () => {
+            const searchButton = findElementByInnerText("Search");
+            searchButton.click();
+        };
 
-const loadCSS = (url) => {
-  const link = document.createElement("link");
-  link.rel = "stylesheet";
-  link.href = url;
-  document.head.appendChild(link);
-};
+        const loadScriptAsync = async (url) => {
+            const response = await fetch(url);
+            eval(await response.text());
+        };
 
-const createCSS = (css) => {
-    const style = document.createElement("style");
-    style.type = "text/css";
-    style.appendChild(document.createTextNode(css));
-    document.head.appendChild(style);
-}
+        const loadCSS = (url) => {
+            const link = document.createElement("link");
+            link.rel = "stylesheet";
+            link.href = url;
+            document.head.appendChild(link);
+        };
 
-const convertNormalDropdownsToSelect2 = () => {
-    const dropdowns = window.jQuery(".segment-builder-embed").find("select:not(.select2-hidden-accessible):not(.comparator)");
-    Array.from(dropdowns).forEach((dropdown) => {
-        const isInsideButtonGroup = dropdown.parentElement.className.includes("input-group");
-        console.log("Inside button group", isInsideButtonGroup);
-        if (!isInsideButtonGroup) dropdowns.select2();
-    });
-}
-
-const elementHasEventListenerForEvent = (element, eventName) => {
-    const result = window.jQuery._data(element, "events");
-    return result[eventName]?.length > 0;
-}
-
-if (!searchPanelOpened()) openSearchPanel();
-
-await loadScriptAsync(
-  "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
-);
-await loadScriptAsync(
-  "https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"
-);
-
-console.log("jQuery", window.jQuery);
-console.log("JQuery loaded");
-convertNormalDropdownsToSelect2();
-window.jQuery(".segment-builder-embed").find(".add-button").on("click", convertNormalDropdownsToSelect2);
-setInterval(() => {
-    convertNormalDropdownsToSelect2();
-}, 1000);
-
-loadCSS("https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css");
-createCSS(".select2-container--default .select2-results > .select2-results__options {	max-height: 55vh; }");
-
-setInterval(() => {
-    const dropdowns = window.jQuery(".segment-builder-embed").find("select.select2-hidden-accessible");
-     Array.from(dropdowns).forEach((dropdown) => {
-        if (!elementHasEventListenerForEvent(dropdown, "select2:select")) {
-            window.jQuery(dropdown).on("select2:select", function (e) {
-                console.log("Selected", e);
-                e.currentTarget.dispatchEvent(new Event("change"));
-            });
-            console.log("Added event listener for select2:select event to dropdown", dropdown)
+        const createCSS = (css) => {
+            const style = document.createElement("style");
+            style.type = "text/css";
+            style.appendChild(document.createTextNode(css));
+            document.head.appendChild(style);
         }
-    });
-}, 1000);
+
+        const convertNormalDropdownsToSelect2 = () => {
+            const dropdowns = window.jQuery(".segment-builder-embed").find("select:not(.select2-hidden-accessible):not(.comparator)");
+            Array.from(dropdowns).forEach((dropdown) => {
+                const isInsideButtonGroup = dropdown.parentElement.className.includes("input-group"); //NH_TODO: Find out why this does not work
+                console.log("Inside button group", isInsideButtonGroup);
+                if (!isInsideButtonGroup) dropdowns.select2();
+            });
+        }
+
+        const elementHasEventListenerForEvent = (element, eventName) => {
+            const result = window.jQuery._data(element, "events");
+            return result[eventName]?.length > 0;
+        }
+
+        if (!searchPanelOpened()) openSearchPanel();
+
+        await loadScriptAsync(
+            "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+        );
+        await loadScriptAsync(
+            "https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"
+        );
+
+        console.log("jQuery", window.jQuery);
+        console.log("JQuery loaded");
+        convertNormalDropdownsToSelect2();
+        window.jQuery(".segment-builder-embed").find(".add-button").on("click", convertNormalDropdownsToSelect2);
+        setInterval(() => {
+            convertNormalDropdownsToSelect2();
+        }, 1000);
+
+        loadCSS("https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css");
+        createCSS(".select2-container--default .select2-results > .select2-results__options {	max-height: 55vh; }");
+
+        setInterval(() => {
+            const dropdowns = window.jQuery(".segment-builder-embed").find("select.select2-hidden-accessible");
+            Array.from(dropdowns).forEach((dropdown) => {
+                if (!elementHasEventListenerForEvent(dropdown, "select2:select")) {
+                    window.jQuery(dropdown).on("select2:select", function (e) {
+                        console.log("Selected", e);
+                        e.currentTarget.dispatchEvent(new Event("change"));
+                    });
+                    console.log("Added event listener for select2:select event to dropdown", dropdown)
+                }
+            });
+        }, 1000);
+    }, 1000);
+})();
